1120b76612e52cf6ef8f55bdf3a83a30f730c169
Istinger <jossuepatric@hotmail.com>
2024-11-26 17:26:10 -0500

    Initial commit

---
+++ lib_genericP65v1/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"lib_genericP65v1","l":"Files"},{"p":"lib_genericP65v1","l":"Generic"},{"p":"lib_genericP65v1","l":"Settings"},{"p":"lib_genericP65v1","l":"TestLibreria"}];updateSearchResults();
---
+++ lib_genericP65v1/src/lib_genericP65v1/Settings.java
@@ -0,0 +1,31 @@
+package lib_genericP65v1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface Settings<T> {
+	public final String developer = "Patricio Proaño 1750764175";
+
+	public final List<String> elements = new ArrayList<>();
+
+	public default void addElements(String element) {
+		this.elements.add(element);
+	}
+
+	public default void listElements() {
+		for(int i =0; i<elements.size(); i++) {
+			System.out.printf("%d.- %s",(i+1),elements.get(i));
+		}
+	}
+
+	public default T searchElement(T...elements) {
+		T element = elements[0];
+		for(int i = 1; i<elements.length; i++) {
+			if(element.toString().equals(element.toString())) {
+				return element;
+			}
+		}
+		return null;
+	}
+
+}
